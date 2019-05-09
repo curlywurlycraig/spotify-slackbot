00734ec680462078c6f59cbbbc3d8064ab105eb4
Craig Wilkinson <craig@dangerfarms.com>
2019-05-09 22:26:12 +0100

    Add scheduled lambda to stack

--- lib/spotify-slackbot-stack.ts
+++ lib/spotify-slackbot-stack.ts
@@ -1,9 +1,23 @@
 import cdk = require('@aws-cdk/cdk');
+import lambda = require('@aws-cdk/aws-lambda');
+import events = require('@aws-cdk/aws-events');
+import event_targets = require('@aws-cdk/aws-events-targets');
 
 export class SpotifySlackbotStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const spotify_lambda = new lambda.Function(this, 'SpotifyCheckerLambda', {
+      code: lambda.Code.directory('spotify-checker'),
+      handler: 'index.main',
+      runtime: lambda.Runtime.NodeJS810
+    });
+
+    const spotify_lambda_target = new event_targets.LambdaFunction(spotify_lambda);
+
+    new events.EventRule(this, 'EveryFiveMinutesEventRule', {
+      scheduleExpression: "rate(5 minutes)",
+      targets: [spotify_lambda_target]
+    });
   }
 }
---
+++ spotify-checker/index.ts
@@ -0,0 +1,3 @@
+export const main = async (event: any = {}): Promise<any> => {
+    console.log(JSON.stringify(event, null, 4));
+}
