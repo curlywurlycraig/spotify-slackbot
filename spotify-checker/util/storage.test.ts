958531ad2c2ad4ece0f601503ed57d2592664e98
Craig Wilkinson <craig@dangerfarms.com>
2019-05-14 10:45:35 +0100

    Begin implementing get last added timestamp

--- spotify-checker/util/storage.test.ts
+++ spotify-checker/util/storage.test.ts
@@ -1,13 +1,19 @@
-// import aws = require('aws-sdk');
+import aws = require('aws-sdk');
 import * as storage from './storage';
 
 describe('getLastAddedTimestamp', () => {
+    let mockAws;
+
     beforeEach(() => {
-        return;
+        mockAws = jest.mock('aws');
+        mockAws.DynamoDB = jest.fn();
     });
 
-    it('should return null when no timestamp is stored', () => {
-        const result = storage.getLastAddedTimestamp();
+    it('should return null when no timestamp is stored', async () => {
+
+
+        const result = await storage.getLastAddedTimestamp();
+
         expect(result).toEqual(null);
-    })
+    });
 })
--- spotify-checker/util/storage.ts
+++ spotify-checker/util/storage.ts
@@ -1,8 +1,22 @@
-const AWS = require('aws-sdk');
+import AWS = require('aws-sdk');
 
 AWS.config.update({region: process.env.REGION})
 const dynamodb = new AWS.DynamoDB();
 
-export const getLastAddedTimestamp = () => {
-    return null;
+export const getLastAddedTimestamp = async () => {
+    return new Promise((resolve, reject) => {
+        dynamodb.getItem({
+            TableName: process.env.TABLE_NAME,
+            Key: {
+                Id: { S: "LastTrackAddedAt" },
+            }
+        }, (err, data) => {
+            if (err) {
+                console.error("Failed to get the last track added time: ", err);
+                reject(null);
+            }
+
+            resolve(data.Item.value);
+        });
+    });
 }
